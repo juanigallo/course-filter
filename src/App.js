374c00b887f4cadc25657f9c87e14a093ca6bf1b
Juani Gallo <ji.gallo93@gmail.com>
2020-11-19 22:06:01 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.scss";
+import { useState } from "react";
 
 function App() {
+  const courses = [
+    {
+      name: "HTML y CSS",
+      color: "rgb(47, 164, 171)",
+      img:
+        "https://courseit-statics.nyc3.digitaloceanspaces.com/icons/logos/html-css.png",
+      desc:
+        "Aprendé a estructurar páginas web con HTML y desatá todo su potencial visual con CSS siguiendo los estándares de la industria"
+    },
+    {
+      name: "Javascript",
+      color: "rgb(222, 145, 3)",
+      img:
+        "https://courseit-statics.nyc3.digitaloceanspaces.com/icons/logos/javascript.png",
+      desc:
+        "Aprendé a programar junto al lenguaje que domina el mundo del desarrollo web"
+    }
+  ];
+
+  const [filteredCourses, setFilteredCourses] = useState(courses);
+
+  function handleChange(e) {
+    const { value } = e.target;
+
+    if (value == "") {
+      setFilteredCourses(courses);
+    }
+
+    const filtered = courses.filter((course) => {
+      return course.name.toLowerCase().includes(value.toLowerCase());
+    });
+
+    setFilteredCourses(filtered);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputContainer">
+        <input
+          type="text"
+          placeholder="Empeza a tipear el nombre de un curso"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="cardsContainer">
+        {filteredCourses.map((course, key) => {
+          return (
+            <div className="cardContainer" key={key}>
+              <div
+                style={{
+                  backgroundColor: course.color,
+                  backgroundImage: `url(${course.img})`
+                }}
+                className="imgContainer"
+              ></div>
+              <h1 className="title">{course.name}</h1>
+              <p className="desc">{course.desc}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
